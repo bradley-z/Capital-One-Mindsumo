ae8300c5c908ae9eb19cabc6d77369923d845c66
Bradley Zhou <bradleyz@andrew.cmu.edu>
2018-06-09 23:33:26 -0400

    basic search function

--- gpo.py
+++ gpo.py
@@ -1,5 +1,9 @@
-from mygpoclient import simple
+from mygpoclient import simple, public
 
-def search():
-    client = simple.SimpleClient('john', 'secret')
-search()
+def search(search_term):
+    client = public.PublicClient()
+
+    search_results = client.search_podcasts(search_term)
+
+def subscriptions():
+    client = simple.SimpleClient('bradleyzhou','3qPB7~e>VR`/p?&S')
