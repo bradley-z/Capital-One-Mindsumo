a3df3d0d3d1ae6f63adb49693eec4923e430f8ed
Bradley Zhou <bradleyz@andrew.cmu.edu>
2018-06-09 22:33:07 -0400

    added routes to each page

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ app.debug = True
 def index():
     return render_template('home.html')
 
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/subscriptions')
+def subscriptions():
+    return render_template('subscriptions.html')
+
+@app.route('smartsearch')
+def smartsearch():
+    return render_template('smartsearch.html')
+
+@app.route('smartsort')
+def smartsort():
+    return render_template('smartsort.html')
+
 if __name__ == '__main__':
     app.run()
